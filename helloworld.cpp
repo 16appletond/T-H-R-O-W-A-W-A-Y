57970ef33c5987665c82cd191385f97465903c3a
Dylan Appleton <dappleto@usc.edu>
2017-01-11 17:48:41 -0800

    added more sadness

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,7 @@ int main (int argc, char* argv[])
 {
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "cs 104 is hard :(" << std::endl;
+	std::cout << "cs 104 is still hard :(" << std::endl;
 
 	return 0 ;
 }
